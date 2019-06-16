2cce04bc914e6adc379c729449fa0f785535106a
Eduardo Barbosa <herulume@protonmail.com>
2019-06-16 11:12:42 +0100

    Basic i3 config

--- install.sh
+++ install.sh
@@ -3,11 +3,14 @@
 sudo pacman -S git fzf neovim tmux kitty ttf-dejavu stylish-haskell
 
 mkdir -p ~/.config/nvim/
+mkdir ~/.i3
+
 
 sudo pip3 install --upgrade neovim
 
 cp tmux.conf ~/.tmux.conf
 cp init.vim ~/.config/nvim/init.vim
 cp kitty.conf ~/.config/kitty/
+cp config ~/.i3/config
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
