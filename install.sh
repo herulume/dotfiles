ac3fdc615c3626bffb1c0a9fb9fae030f778534c
herulume <herulume@protonmail.com>
2019-06-26 11:33:08 +0100

    m

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ cp kitty.conf ~/.config/kitty/
 cp config ~/.i3/config
 cp .bashrc ~/.bashrc
 cp .bash_profile ~/.bash_profile
-
+ln -sfT "$HOME/dev/dotfiles/gitconfig" "$HOME/.gitconfig"
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.2
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 curl -sSL https://get.haskellstack.org/ | sh
