8d5a9080412394287fbf8178df34f5d7944ace11
herulume <herulume@protonmail.com>
2020-03-01 22:21:58 +0000

    m

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ ln -sfT "$HOME/dev/dotfiles/gitconfig"    "$HOME/.gitconfig"
 ln -sfT "$HOME/dev/dotfiles/cwmrc"        "$HOME/.cwmrc"
 ln -sfT "$HOME/dev/dotfiles/xsession"     "$HOME/.xsession"
 ln -sfT "$HOME/dev/dotfiles/init.vim"     "$HOME/.config/nvim/init.vim"
-ln -sfT "$HOME/dev/dotfile/tmux.conf"     "$HOME/.tmux.conf"
+ln -sfT "$HOME/dev/dotfiles/tmux.conf"     "$HOME/.tmux.conf"
 
 
 git clone https://github.com/asdf-vm/asdf.git "$HOME/.asdf" --branch v0.7.2
