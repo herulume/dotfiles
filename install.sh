0101b1f84a4ceb703c8dcecc7f4d52bf3c5d47ce
herulume <herulume@protonmail.com>
2020-03-01 22:33:39 +0000

    yay

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo pacman -Syu
-sudo pacman -S neovim feh dmenu i3status otf-fira-code xorg-xinput arandr cmake libtool ledger tmux light pulsemixer slock yay fzf kitty mpv
+sudo pacman -S neovim feh dmenu i3status otf-fira-code xorg-xinput arandr cmake libtool ledger tmux xorg-xbacklight pulsemixer slock yay fzf kitty mpv
 yay -S cwm
 
 git clone git://git.suckless.org/st "$HOME/dev/st"
