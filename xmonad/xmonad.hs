01fd9a329c2a1b6c98df8a03d91dbf5e588f09c7
herulume <herulume@protonmail.com>
2020-08-24 15:24:36 +0100

    up

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,28 +1,119 @@
+    -- Base
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Layout.LayoutModifier
+import System.IO (hPutStrLn)
+import System.Exit (exitSuccess)
+import qualified XMonad.StackSet as W
+
+    -- Actions
+import XMonad.Actions.CopyWindow (kill1, killAllOtherCopies)
+import XMonad.Actions.CycleWS (moveTo, shiftTo, WSType(..), nextScreen, prevScreen)
+import XMonad.Actions.GridSelect
+import XMonad.Actions.MouseResize
+import XMonad.Actions.Promote
+import XMonad.Actions.RotSlaves (rotSlavesDown, rotAllDown)
+import qualified XMonad.Actions.TreeSelect as TS
+import XMonad.Actions.WindowGo (runOrRaise)
+import XMonad.Actions.WithAll (sinkAll, killAll)
+import qualified XMonad.Actions.Search as S
+
+    -- Data
+import Data.Char (isSpace)
+import Data.Monoid
+import Data.Maybe (isJust)
+import Data.Tree
+import qualified Data.Map as M
+
+    -- Hooks
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, xmobarPP, xmobarColor, shorten, PP(..))
+import XMonad.Hooks.EwmhDesktops  -- for some fullscreen events, also for xcomposite in obs.
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks, ToggleStruts(..))
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
+import XMonad.Hooks.ServerMode
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.WorkspaceHistory
 import XMonad.Hooks.ManageDocks
+
+    -- Layouts
+import XMonad.Layout.GridVariants (Grid(Grid))
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.Spiral
 import XMonad.Layout.ResizableTile
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
+
+    -- Layouts modifiers
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
+import XMonad.Layout.Magnifier
+import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), (??))
+import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Renamed (renamed, Rename(Replace))
+import XMonad.Layout.ShowWName
 import XMonad.Layout.Spacing
-import XMonad.Util.EZConfig
+import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
+import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
+import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
+
+    -- Prompt
+import XMonad.Prompt
+import XMonad.Prompt.Input
+import XMonad.Prompt.FuzzyMatch
+import XMonad.Prompt.Man
+import XMonad.Prompt.Pass
+import XMonad.Prompt.Shell (shellPrompt)
+import XMonad.Prompt.Ssh
+import XMonad.Prompt.XMonad
+import Control.Arrow (first)
+
+    -- Utilities
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.NamedScratchpad
+import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
 import XMonad.Util.SpawnOnce
 
 
-main :: IO ()
-main = xmonad $ (docks myConfig)
 
-myConfig = ewmh def { terminal = "urxvtc"
-                    , modMask = mod4Mask
-                    , borderWidth = 0
-                    , focusFollowsMouse = True
-                    , layoutHook = myLayoutHook
-                    , startupHook = myStartupHook
-                    } `additionalKeysP` myKeys
+myModMask :: KeyMask
+myModMask = mod4Mask
+
+
+myFont :: String
+myFont = "xft:Fira Code Font:bold:size=9:antialias=true:hinting=true"
+
+myTerminal :: String
+myTerminal = "urxvt"
+
+myBorderWidth :: Dimension
+myBorderWidth = 2          -- Sets border width for windows
+
+myNormColor :: String
+myNormColor   = "#292d3e"  -- Border color of normal windows
+
+myFocusColor :: String
+myFocusColor  = "#bbc5ff"  -- Border color of focused windows
+
+
+myStartupHook :: X ()
+myStartupHook = do
+  spawnOnce "xsetroot -cursor_name left_ptr"
+  spawnOnce "feh --bg-scale ~/Pictures/Wallpapers/wallpaper.jpg"
+  spawnOnce "setxkbmap -option ctrl:nocaps"
+  spawnOnce "xset b off"
+  spawnOnce "xmobar ~/.xmonad/.xmobarrc"
+  spawnOnce "tapping"
+  spawnOnce "urxvtd -q -o -f"
+  spawnOnce "emacs --daemon"
+
+
 
 myKeys :: [(String, X ())]
 myKeys = [ ("M-r", spawn "rofi -show run")
          , ("M-w", spawn "rofi -show window")
+
+         , ("M-<Return>", spawn "urxvtc")
+
          , ("<XF86MonBrightnessDown>", spawn "xbacklight -dec 5")
          , ("<XF86MonBrightnessUp>", spawn "xbacklight -inc 5")
          , ("<XF86AudioLowerVolume>", spawn "amixer set Master 5%-")
@@ -33,35 +124,125 @@ myKeys = [ ("M-r", spawn "rofi -show run")
          , ("<Print>", spawn "sh -c 'import -window root ~/Desktop/screen_shot_$(date --iso-8601=seconds).png'")
          , ("M-<Print>", spawn "sh -c 'import ~/Desktop/screen_shot_$(date --iso-8601=seconds).png'")
          , ("M-C-<Print>", spawn "sh -c 'import -window $(xprop -root | grep \"_NET_ACTIVE_WINDOW(WINDOW)\" | sed -e \"s/.* # //g\") ~/Desktop/screen_shot_$(date --iso-8601=seconds).png'")
-         , ("M-x", spawn "pkill xmobar")
-         , ("M-a", sendMessage MirrorShrink)
-         , ("M-z", sendMessage MirrorExpand)
+
+          -- Floating windows
+        , ("M-f", sendMessage (T.Toggle "floats"))       -- Toggles my 'floats' layout
+        , ("M-<Delete>", withFocused $ windows . W.sink) -- Push floating window back to tile
+        , ("M-S-<Delete>", sinkAll)                      -- Push ALL floating windows to tile
+
+          -- Windows navigation
+        , ("M-m", windows W.focusMaster)     -- Move focus to the master window
+        , ("M-j", windows W.focusDown)       -- Move focus to the next window
+        , ("M-k", windows W.focusUp)         -- Move focus to the prev window
+        --, ("M-S-m", windows W.swapMaster)    -- Swap the focused window and the master window
+        , ("M-S-j", windows W.swapDown)      -- Swap focused window with next window
+        , ("M-S-k", windows W.swapUp)        -- Swap focused window with prev window
+        , ("M-<Backspace>", promote)         -- Moves focused window to master, others maintain order
+        , ("M1-S-<Tab>", rotSlavesDown)      -- Rotate all windows except master and keep focus in place
+        , ("M1-C-<Tab>", rotAllDown)         -- Rotate all the windows in the current stack
+        --, ("M-S-s", windows copyToAll)
+        , ("M-C-s", killAllOtherCopies)
+
+        -- Layouts
+        , ("M-<Tab>", sendMessage NextLayout)                -- Switch to next layout
+        , ("M-C-M1-<Up>", sendMessage Arrange)
+        , ("M-C-M1-<Down>", sendMessage DeArrange)
+        , ("M-<Space>", sendMessage (MT.Toggle NBFULL) >> sendMessage ToggleStruts) -- Toggles noborder/full
+        , ("M-S-<Space>", sendMessage ToggleStruts)         -- Toggles struts
+        , ("M-S-n", sendMessage $ MT.Toggle NOBORDERS)      -- Toggles noborder
+        , ("M-<KP_Multiply>", sendMessage (IncMasterN 1))   -- Increase number of clients in master pane
+        , ("M-<KP_Divide>", sendMessage (IncMasterN (-1)))  -- Decrease number of clients in master pane
+        , ("M-S-<KP_Multiply>", increaseLimit)              -- Increase number of windows
+        , ("M-S-<KP_Divide>", decreaseLimit)                -- Decrease number of windows
+
+        , ("M-h", sendMessage Shrink)                       -- Shrink horiz window width
+        , ("M-l", sendMessage Expand)                       -- Expand horiz window width
+        , ("M-C-j", sendMessage MirrorShrink)               -- Shrink vert window width
+        , ("M-C-k", sendMessage MirrorExpand)               -- Exoand vert window width
+
+
          , ("C-e e", spawn "emacsclient -c -a ''")                            -- start emacs
          , ("C-e b", spawn "emacsclient -c -a '' --eval '(ibuffer)'")         -- list emacs buffers
          , ("C-e d", spawn "emacsclient -c -a '' --eval '(dired nil)'")       -- dired emacs file manager
          ]
 
-myStartupHook :: X ()
-myStartupHook = do
-  spawnOnce "xsetroot -cursor_name left_ptr"
-  spawnOnce "feh --bg-scale ~/Pictures/Wallpapers/wallpaper.jpg"
-  spawnOnce "msm_notifier"
-  spawnOnce "start-pulseaudio-x11"
-  spawnOnce "setxkbmap -option ctrl:nocaps"
-  spawnOnce "xset b off"
-  spawnOnce "xmobar ~/.xmonad/.xmobarrc"
-  spawnOnce "tapping"
-  spawnOnce "urxvtd -q -o -f"
-  spawnOnce "emacs --daemon"
+mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 
+-- Below is a variation of the above except no borders are applied
+-- if fewer than two windows. So a single window has no gaps.
+mySpacing' :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+mySpacing' i = spacingRaw True (Border i i i i) True (Border i i i i) True
 
-myLayoutHook = mySpacing $ avoidStruts (ResizableTall 1 (3/100) (1/2) [] ||| Full)
+-- Defining a bunch of layouts, many that I don't use.
+tall     = renamed [Replace "tall"]
+           $ limitWindows 12
+           $ mySpacing 8
+           $ ResizableTall 1 (3/100) (1/2) []
+magnify  = renamed [Replace "magnify"]
+           $ magnifier
+           $ limitWindows 12
+           $ mySpacing 8
+           $ ResizableTall 1 (3/100) (1/2) []
+monocle  = renamed [Replace "monocle"]
+           $ limitWindows 20 Full
+floats   = renamed [Replace "floats"]
+           $ limitWindows 20 simplestFloat
+grid     = renamed [Replace "grid"]
+           $ limitWindows 12
+           $ mySpacing 8
+           $ mkToggle (single MIRROR)
+           $ Grid (16/10)
+spirals  = renamed [Replace "spirals"]
+           $ mySpacing' 8
+           $ spiral (6/7)
+threeCol = renamed [Replace "threeCol"]
+           $ limitWindows 7
+           $ mySpacing' 4
+           $ ThreeCol 1 (3/100) (1/2)
+threeRow = renamed [Replace "threeRow"]
+           $ limitWindows 7
+           $ mySpacing' 4
+           -- Mirror takes a layout and rotates it by 90 degrees.
+           -- So we are applying Mirror to the ThreeCol layout.
+           $ Mirror
+           $ ThreeCol 1 (3/100) (1/2)
+tabs     = renamed [Replace "tabs"]
+           -- I cannot add spacing to this layout because it will
+           -- add spacing between window and tabs which looks bad.
+           $ tabbed shrinkText myTabConfig
+  where
+    myTabConfig = def { fontName            = "xft:Fira Code Font:regular:pixelsize=11"
+                      , activeColor         = "#292d3e"
+                      , inactiveColor       = "#3e445e"
+                      , activeBorderColor   = "#292d3e"
+                      , inactiveBorderColor = "#292d3e"
+                      , activeTextColor     = "#ffffff"
+                      , inactiveTextColor   = "#d0d0d0"
+                      }
 
-mySpacing = spacingRaw True myBorder True myBorder True
+-- The layout hook
+myLayoutHook = avoidStruts $ mouseResize $ windowArrange $ T.toggleLayouts floats $
+               mkToggle (NBFULL ?? NOBORDERS ?? EOT) myDefaultLayout
+             where
+               -- I've commented out the layouts I don't use.
+               myDefaultLayout =     noBorders tall
+                                 -- ||| magnify
+                                 -- ||| noBorders monocle
+                                 -- ||| floats
+                                 -- ||| grid
+                                 ||| noBorders tabs
+                                 -- ||| spirals
+                                 -- ||| threeCol
+                                 -- ||| threeRow
 
-myBorder :: Border
-myBorder = Border { top = 5
-                  , bottom = 5
-                  , right = 5
-                  , left = 5
-                  }
+myConfig = ewmh def { terminal = myTerminal
+                    , modMask = myModMask
+                    , borderWidth = myBorderWidth
+                    , focusFollowsMouse = True
+                    , layoutHook = myLayoutHook
+                    , startupHook = myStartupHook
+                    } `additionalKeysP` myKeys
+
+main :: IO ()
+main = xmonad $ docks myConfig
